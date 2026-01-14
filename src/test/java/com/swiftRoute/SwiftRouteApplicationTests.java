fc9113237fbd2e15e08857a371975de2ccd01811
Santosh <sbhor747@gmail.com>
2026-01-14 23:11:34 +0530

    initialize SwiftRoute

---
+++ src/test/java/com/swiftRoute/SwiftRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiftRoute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftRouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
