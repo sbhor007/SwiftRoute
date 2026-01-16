5800c74c939f4f4c461aa72260c230bae5593e2c
Santosh <sbhor747@gmail.com>
2026-01-16 23:50:25 +0530

    implement UserDetail interface

--- src/main/java/com/swiftRoute/entity/User.java
+++ src/main/java/com/swiftRoute/entity/User.java
@@ -1,6 +1,8 @@
 package com.swiftRoute.entity;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 import com.swiftRoute.enums.UserRole;
@@ -10,6 +12,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "users")
@@ -17,7 +22,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -55,5 +60,15 @@ public class User {
     @PreUpdate
     void onUpdate() {
         updatedAt = LocalDateTime.now();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
     }
 }
