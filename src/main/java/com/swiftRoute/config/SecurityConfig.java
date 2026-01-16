294c7136851fd2ddcce9844ece7b256bd9a79799
Santosh <sbhor747@gmail.com>
2026-01-17 05:28:58 +0530

    permit '/actuator/health' enpoint to accessible for all

--- src/main/java/com/swiftRoute/config/SecurityConfig.java
+++ src/main/java/com/swiftRoute/config/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig implements WebMvcConfigurer {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtFilterChain, UsernamePasswordAuthenticationFilter.class);
 
