2c8f55bc99ddd0b3236ddffb9daa116b96d2862b
Santosh <sbhor747@gmail.com>
2026-01-17 19:51:22 +0530

    create login method: add buisness logic of user login and JWT token creation

--- src/main/java/com/swiftRoute/service/AuthService.java
+++ src/main/java/com/swiftRoute/service/AuthService.java
@@ -2,12 +2,21 @@ package com.swiftRoute.service;
 
 import com.swiftRoute.entity.User;
 import com.swiftRoute.enums.UserRole;
+import com.swiftRoute.records.auth.LoginRequest;
+import com.swiftRoute.records.auth.LoginResponse;
 import com.swiftRoute.records.user.RegisterRequest;
 import com.swiftRoute.repository.UserRepository;
+import com.swiftRoute.util.JwtUtil;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -15,6 +24,10 @@ import org.springframework.stereotype.Service;
 public class AuthService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private  final JwtUtil jwtUtil;
+
+    @Transactional
     public void register(RegisterRequest registerRequest){
         try{
             if(emailAlreadyExist(registerRequest.email(),registerRequest.role())){
@@ -34,6 +47,31 @@ public class AuthService {
             throw new RuntimeException(e.getMessage());
         }
     }
+
+    public Optional<LoginResponse> login(LoginRequest request) {
+        log.info("Login attempt for user: {}", request.username());
+
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(request.username(), request.password())
+            );
+
+            User user = (User) authentication.getPrincipal();
+
+            if (user.getRole() != request.role()) {
+                log.error("Role mismatch: expected {}, found {}", request.role(), user.getRole());
+                throw new RuntimeException("Invalid Credentials");
+            }
+
+            String token = jwtUtil.generatAccessToken(user);
+            return Optional.of(new LoginResponse(token, user.getRole()));
+
+        } catch (Exception e) {
+            log.error("Login failed for user: {}", request.username(), e);
+            throw new RuntimeException("Invalid Credentials");
+        }
+    }
+
 
     private boolean emailAlreadyExist(String email, UserRole role){
             log.info("check email and Role already exist");
