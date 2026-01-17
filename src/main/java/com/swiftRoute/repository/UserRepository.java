2fb6098e310e2bd1239f3be4279e2cbc3711c0c1
Santosh <sbhor747@gmail.com>
2026-01-17 20:53:49 +0530

    crete custome query: findByEmailForAuth

--- src/main/java/com/swiftRoute/repository/UserRepository.java
+++ src/main/java/com/swiftRoute/repository/UserRepository.java
@@ -4,7 +4,9 @@ import java.util.Optional;
 import java.util.UUID;
 
 import com.swiftRoute.enums.UserRole;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.swiftRoute.entity.User;
@@ -14,5 +16,7 @@ import com.swiftRoute.entity.User;
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByEmail(String email);
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    Optional<User> findByEmailForAuth(@Param("email") String email);
     Optional<Boolean> existsByEmailAndRole(String email, UserRole role);
 }
