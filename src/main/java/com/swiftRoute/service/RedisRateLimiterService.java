8e3713abff47bc680dc383c09352fa52085d81aa
Santosh <sbhor747@gmail.com>
2026-01-28 23:27:33 +0530

    Documented redisReateLimiter

--- src/main/java/com/swiftRoute/service/RedisRateLimiterService.java
+++ src/main/java/com/swiftRoute/service/RedisRateLimiterService.java
@@ -10,25 +10,31 @@ import java.util.concurrent.TimeUnit;
 @AllArgsConstructor
 public class RedisRateLimiterService {
     private final RedisTemplate<String,Object> redisTemplate;
-
+    /**
+     * Rate Limiter using Redis Sorted Set
+     * @param key Unique key for the user/requester
+     * @param limitRequests Maximum number of requests allowed
+     * @param ttl Time window in seconds
+     * @return true if request is allowed, false otherwise
+     */
     public boolean allowRequest(String key, int limitRequests, int ttl){
 
-        /*remove old request*/
+        /**remove old request*/
         long now  = System.currentTimeMillis();
         redisTemplate.opsForZSet()
                 .removeRangeByScore(key,0,now-ttl * 1000L);
 
-        /* Count Number of Requests */
+        /** Count Number of Requests */
         Long count = redisTemplate.opsForZSet().zCard(key);
         if(count != null && count >= limitRequests){
             return false;
         }
 
-        /* add current request */
+        /** add current request */
         redisTemplate.opsForZSet()
                 .add(key,String.valueOf(now),now);
 
-        /* set expiry */
+        /** set expiry */
         redisTemplate.expire(key,ttl, TimeUnit.SECONDS);
 
         return true;
